8b4f021be742f9e29cfd3e48ceb09047403e9130
Saikrishna Bairamoni <84093461+SaikrishnaBairamoni@users.noreply.github.com>
2023-03-27 16:29:47 -0400

    add connection block to ssh

--- tf-example/main.tf
+++ tf-example/main.tf
@@ -19,6 +19,12 @@ resource "aws_instance" "carmacloud-test" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
   key_name      = "myJune222Key"  
+  connection {
+    type     = "ssh"
+    user     = "root"
+    private_key = "${file("/tf-example/myJune222Key.pem")}"
+    host     = self.public_ip
+  }    
   provisioner "file" {
     source      = "cc.sh"
     destination = "/home/ubuntu/cc.sh"
