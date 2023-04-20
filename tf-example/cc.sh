a202fda274fe94d3240f8b2407c0143b132576cb
Saikrishna Bairamoni <84093461+SaikrishnaBairamoni@users.noreply.github.com>
2023-04-20 14:13:26 -0400

    fix

--- tf-example/cc.sh
+++ tf-example/cc.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 sudo apt update
-sudo apt install openjdk-11-jdk-headless
 sudo source .bashrc 
 sudo mkdir tmp && cd tmp
 wget https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.34/bin/apache-tomcat-9.0.34.tar.gz
