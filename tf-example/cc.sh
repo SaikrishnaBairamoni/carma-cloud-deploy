487f7d0884843a5e09b1c87456257c7e6a9a59aa
Saikrishna Bairamoni <84093461+SaikrishnaBairamoni@users.noreply.github.com>
2023-04-20 14:01:38 -0400

    test

--- tf-example/cc.sh
+++ tf-example/cc.sh
@@ -6,10 +6,3 @@ sudo mkdir tmp && cd tmp
 wget https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.34/bin/apache-tomcat-9.0.34.tar.gz
 tar -xzf apache-tomcat-9.0.34.tar.gz
 mv apache-tomcat-9.0.34 tomcat
-rm -rf tomcat/webapps/ROOT
-rm -rf tomcat/webapps/docs
-rm -rf tomcat/webapps/examples
-rm -rf tomcat/webapps/host-manager
-rm -rf tomcat/webapps/manager
-rm -f apache-tomcat-9.0.34.tar.gz
-git clone https://github.com/usdot-fhwa-stol/carma-cloud.git
